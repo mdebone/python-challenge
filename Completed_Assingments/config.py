66f9313ad54d357cba6914f91917236eb737874f
Matt DeBone <mdebone@gmail.com>
2021-11-09 20:59:35 -0600

    updated completed assignments

---
+++ Completed_Assingments/config.py
@@ -0,0 +1,7 @@
+weather_api_key = "6a3af63ab782549e7a87a984287f1313"
+omdb_api_key = "599a01dd"
+nyt_api_key = "D8BcRuwtjSNbGc9MJ8zZDjgLJ58donyw"
+
+#gkey = "YOUR_KEY_HERE"
+google_api_key = "AIzaSyCFJZ0BRBe2aaboU1jH88dxthMfPcDSK1g"
+census_api_key = "bcd6643c81025995f435ee6ad9cc4b20b508c059"
